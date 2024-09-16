d33cf7a1b4e5ec960aa805ed4433eae2002eac11
Shashika Kulasekara <shashika.t@hotmail.com>
2024-09-16 21:43:52 +0530

    Setup folder structure
    Setup folder structure.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Style/index.css';
+import './actions/styles/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
