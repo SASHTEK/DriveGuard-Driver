1d3842382e50dae5efb98aaa5d287d5b61a63e6b
Shashika Kulasekara <shashika.t@hotmail.com>
2024-09-16 22:00:29 +0530

    Minor corrections -page path
    correction to page paths.

--- src/actions/components/navbar/TabNavigation.js
+++ src/actions/components/navbar/TabNavigation.js
@@ -2,10 +2,10 @@ const tabNavigation = () => {
     return (  
         <div className='tabNavigationButtons'>
             <div className="tabNavLinks">
-                <a href="./actions/views/Home">Home</a>
-                <a href="./actions/views/New">New</a>
-                <a href="./actions/views/History">History</a>
-                <a href="./actions/views/Settings">Settings</a>
+                <a href="./actions/views/home/Home">Home</a>
+                <a href="./actions/views/new/New">New</a>
+                <a href="./actions/views/history/History">History</a>
+                <a href="./actions/views/settings/Settings">Settings</a>
             </div>
         </div>
     );
