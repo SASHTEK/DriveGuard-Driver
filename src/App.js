3f0ec10657b5323e57d2733906ddc086b836a622
Shashika Kulasekara <shashika.t@hotmail.com>
2024-09-17 14:43:42 +0530

    Interface modification
    Interface scaling to  240x426.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import LoginSignup from './actions/components/auth/LoginSignup';
+import Home from './actions/views/home/Home';
 function App() {
   return (
     <div className="App">
       <div className="content">
         <LoginSignup/>
+        <Home/>
 
       </div>
     </div>
--- src/actions/views/home/Home.js
+++ src/actions/views/home/Home.js
@@ -1,6 +1,7 @@
+import './Home.css';
 const Home = () => {
     return ( 
-        <div className="home">
+        <div className="container">
             
         </div>
      );
