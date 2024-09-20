4c1d499a55ee8363bd9646e56570c6516951ec2c
Shashika Kulasekara <shashika.t@hotmail.com>
2024-09-20 12:01:40 +0530

    UI Improvements

--- src/actions/views/history/History.js
+++ src/actions/views/history/History.js
@@ -1,11 +1,14 @@
 import './History.css';
+import HeaderBox from '../../components/objects/HeaderBox/HeaderBox';
 import TabNavigation from '../../components/navbar/TabNavigation';
 const Histroty = () => {
     return ( 
         <div className="container">
-            <tab className="navposition-history">
-                <TabNavigation bghistory={"rgb(10, 55, 202)"}/>
-            </tab>
+            <HeaderBox headertext={"Offense History"}/>
+            <div className='page-content'>
+                
+            </div>
+            <TabNavigation bghistory={"rgb(10, 55, 202)"}/>
         </div>
      );
 }
