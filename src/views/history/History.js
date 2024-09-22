a3ea0f1b3fe5b796c30703ff3f6d43fc9be3a156
Shashika Kulasekara <shashika.t@hotmail.com>
2024-09-22 13:48:22 +0530

    Minor UI improvements

--- src/views/history/History.js
+++ src/views/history/History.js
@@ -1,78 +1,3 @@
-// import './History.css';
-// import HeaderBox from '../../components/objects/HeaderBox/HeaderBox';
-// import TabNavigation from '../../components/navbar/TabNavigation';
-// import React, { useState } from 'react';
-// import DateTimePicker from 'react-datetime-picker';
-
-// //Browse History List
-// const options = [
-//     { value: 'Fine pay', label: 'Fine pay' },
-//     { value: 'Offense 1', label: 'Offense 1' },
-//     { value: 'Offenses 2', label: 'Offenses 2' },
-//     { value: 'Offenses 3', label: 'Offenses 3' },
-//   ];
-
-// const Histroty = () => {
-
-//     //Date Time Picker
-//     function DateRangePicker() {
-//         const [fromDate, setFromDate] = useState(new Date());
-//         const [toDate, setToDate] = useState(new Date());
-      
-//         const handleFromDateChange = (date) => {
-//           setFromDate(date);
-//         };
-      
-//         const handleToDateChange = (date) => {
-//           setToDate(date);
-//         };
-//     }
-
-//     return ( 
-//         <div className="container">
-//             <HeaderBox headertext={"Offense History"}/>
-//             <div className='page-content-history'>
-
-//                 <div className='indicator'>
-//                     <h2>Offense Summary</h2>
-//                     <div className='offense-summary'>
-//                         <p>This is a test attempt</p>
-//                         <p>This is the final attempt</p>
-//                     </div>
-//                 </div>
-
-//                 <div className='history-list-view'>
-//                     <h2>Browse History</h2>
-//                     <div className='history-list-components'>
-//                         <div className='date-range'>
-//                             <div>
-//                                 <label>From Date:</label>
-//                                 <DateTimePicker value={fromDate} onChange={handleFromDateChange} />
-//                             </div>
-//                             <div>
-//                                 <label>To Date:</label>
-//                                 <DateTimePicker value={toDate} onChange={handleToDateChange} />
-//                             </div>
-//                         </div>
-//                         <select>{options.map((option) => (
-//                             <option key={option.value} value={option.value}>{option.label}</option>))}
-//                         </select>
-//                         <button>Browse</button>
-//                     </div>
-//                     <div className='history-list-display'>
-//                         <div className='history-list-display-box>'>
-
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//             <TabNavigation bghistory={"rgb(10, 55, 202)"}/>
-//         </div>
-//      );
-// }
- 
-// export default Histroty;
-
 import './History.css';
 import React, { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker';
@@ -102,6 +27,7 @@ const History = () => {
     setToDate(date);
   };
 
+
   return (
     <div className="container">
 
@@ -112,8 +38,7 @@ const History = () => {
             <div className="indicator">
                 <h2>Offense Summary</h2>
                 <div className="offense-summary">
-                    <p>This is a test attempt</p>
-                    <p>This is the final attempt</p>
+                    {/* Display offense summary list here */}
                 </div>
             </div>
 
