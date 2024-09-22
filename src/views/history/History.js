c4a4af86aeedd89731442ebc270e9a5da4fc4f05
Shashika Kulasekara <shashika.t@hotmail.com>
2024-09-22 08:52:42 +0530

    Implement - History page
    Initialize main components.

--- src/views/history/History.js
+++ src/views/history/History.js
@@ -6,8 +6,23 @@ const Histroty = () => {
         <div className="container">
             <HeaderBox headertext={"Offense History"}/>
             <div className='page-content-history'>
+
                 <div className='indicator'>
+                    <h2>Offense Summary</h2>
+                    <div className='offense-summary'>
+                        <p>This is a test</p>
+                        <p>This is a test</p>
+                        <p>This is a test</p>
+                        <p>This is a final</p>
+                    </div>
                 </div>
+
+                <div className='history-list-view'>
+                    <h2>Browse History</h2>
+                    
+
+                </div>
+
             </div>
             <TabNavigation bghistory={"rgb(10, 55, 202)"}/>
         </div>
