2e483f153a7d23ce944cdd683d84a91673d6919d
Shashika Kulasekara <shashika.t@hotmail.com>
2024-10-02 14:14:16 +0530

    Update App.js
    Fixed the router confliction.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import LoginSignup from './actions/components/auth/LoginSignup';
-import Home from './actions/views/home/Home';
-import New from './actions/views/new/New';
-import History from './actions/views/history/History';
-import Settings from './actions/views/settings/Settings';
-=======
-
 import LoginSignup from './components/auth/LoginSignup';
 import './App.css';
 import Home from './views/home/Home';
 import New from './views/new/New';
 import History from './views/history/History';
 import Settings from './views/settings/Settings';
->>>>>>> 63b57cfc1461369be773e0b3a6645bfee898ba49
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
